7bdef37f21dddaba9f54e655836a2bcf17cc7973
ABIKUTTI <31499358+ABIKUTTI@users.noreply.github.com>
2018-01-19 16:30:48 +0530

    Create nk.c

---
+++ nk.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+void main()
+{
+int arr[N];
+int i;
+intsum=0;
+printf("program to find an array of N numbers:");
+for(i=0;i<N;i++)
+{
+printf("enter number.%d:\n",i+1);
+scanf("%d",&arr[i]);
+}
+for(i=0;i<N;i++)
+sum=sum+arr[i];
+}
+printf("sum:5d",sum);
+}
