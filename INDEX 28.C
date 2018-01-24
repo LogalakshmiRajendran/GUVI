bbb3443a155aa831b6b24fcd5c5431b3ec67e48d
ABIKUTTI <31499358+ABIKUTTI@users.noreply.github.com>
2018-01-24 16:37:49 +0530

    Create INDEX 28.C

---
+++ INDEX 28.C
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+int i,int arr[50],num;
+printf("\n enter number of elements:");
+scanf("%d",&num);
+printf("\n enter the values:");
+for(i=0;i<num;i++)
+}
+scanf("%d",&ar[i]);
+
+for(i=0;i<num;i++)
+{
+printf("\narr[%d]=%d",i,arr[i]);
+}
+return(0);
+}
